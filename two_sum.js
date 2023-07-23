2959eba8204b52625b0846493b23e39c1baa388f
Nik <malikhinspb@gmail.com>
2023-07-23 20:54:56 +0300

    optimized two sum solutions

--- two_sum.js
+++ two_sum.js
@@ -1,16 +1,18 @@
 export default twoSum;
 
 function twoSum(arr, target) {
+    //time complexity is N
+    //space complexity is N
     if (arr.length < 2) return null;
 
-    let size = arr.length;
-    for (let i = 0; i < size; i++) {
-        let iVal = arr[i];
-        for (let j = i + 1; j < size; j++) {
-            if (iVal + arr[j] == target) {
-                return [i, j];
-            }
+    const jTargets = new Map();
+    for (let i = 0; i < arr.length; i++) {
+        let jTargetValue = jTargets.get(arr[i]);
+        if (jTargetValue != undefined) {
+            return [jTargetValue, i];
         }
+
+        jTargets.set(target - arr[i], i)
     }
 
     return null;
