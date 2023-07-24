7420042f98ce7d63c4c92c550923c17dc4f9f595
Nik <malikhinspb@gmail.com>
2023-07-24 13:36:13 +0300

    brute force solution containerWithMostWater

--- arrays/containerWithMostWater.js
+++ arrays/containerWithMostWater.js
@@ -1,5 +1,17 @@
 export default containerWithMostWater;
 
-function containerWithMostWater() {
-    
+function containerWithMostWater(arr) {
+    let res = 0;
+    for(let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            let h = Math.min(arr[i], arr[j]);
+            let w = j - i;
+            let temp = h * w;
+            if (temp > res) {
+                res = temp;
+            }
+        }
+    }
+
+    return res;
 }
--- arrays/containerWithMostWater.test.js
+++ arrays/containerWithMostWater.test.js
@@ -1,5 +1,17 @@
 import containerWithMostWater from  "./containerWithMostWater";
 
-test("test containerWithMostWater", () => {
-    expect(containerWithMostWater()).toBe();
+test("when apply [7,1,2,3,9] then 28", () => {
+    expect(containerWithMostWater([7, 1, 2, 3, 9])).toBe(28);
+});
+
+test("when apply [] then 0", () => {
+    expect(containerWithMostWater([])).toBe(0);
+});
+
+test("when apply [7] then 0", () => {
+    expect(containerWithMostWater([7])).toBe(0);
+});
+
+test("when apply [6,9,3,4,5,8] then 32", () => {
+    expect(containerWithMostWater([6, 9, 3, 4, 5, 8])).toBe(32);
 });
