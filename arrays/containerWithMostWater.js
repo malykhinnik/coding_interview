dd047da70ba76bd3aa77fe150f3a3a1676768524
Nik <malikhinspb@gmail.com>
2023-07-25 12:25:44 +0300

    containerWithMostWater two shift solutions

--- arrays/containerWithMostWater.js
+++ arrays/containerWithMostWater.js
@@ -1,17 +1,23 @@
 export default containerWithMostWater;
 
 function containerWithMostWater(arr) {
-    let res = 0;
-    for(let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            let h = Math.min(arr[i], arr[j]);
-            let w = j - i;
-            let temp = h * w;
-            if (temp > res) {
-                res = temp;
-            }
+    let i = 0;
+    let j = arr.length - 1;
+    let maxArea = 0;
+    while (i < j) {
+        const iv = arr[i];
+        const jv = arr[j];
+        const h = Math.min(iv, jv);
+        const w = j - i;
+        const area = h * w;
+        maxArea = Math.max(area, maxArea);
+        if (iv <= jv) {
+            i++;
+        } else {
+            j--;
         }
+
     }
 
-    return res;
+    return maxArea;
 }
--- arrays/containerWithMostWater.test.js
+++ arrays/containerWithMostWater.test.js
@@ -15,3 +15,11 @@ test("when apply [7] then 0", () => {
 test("when apply [6,9,3,4,5,8] then 32", () => {
     expect(containerWithMostWater([6, 9, 3, 4, 5, 8])).toBe(32);
 });
+
+test("when apply [1,2,1] then 2", () => {
+    expect(containerWithMostWater([1, 2, 1])).toBe(2);
+});
+
+test("when apply [2,3,4,5,18,17,6] then 17", () => {
+    expect(containerWithMostWater([2, 3, 4, 5, 18, 17, 6])).toBe(17);
+});
