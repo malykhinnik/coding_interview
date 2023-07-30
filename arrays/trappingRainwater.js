2a2b9d765d1cb5ef96e912e9115eb2228a19269f
Nik <malikhinspb@gmail.com>
2023-07-30 12:32:45 +0300

    trappingRainwater solution

--- arrays/trappingRainwater.js
+++ arrays/trappingRainwater.js
@@ -1,6 +1,32 @@
 export default trappingRainwater;
 
 function trappingRainwater(arr) {
-    let res = 0;
-    return res;
+    let left = 0;
+    let right = arr.length - 1;
+    let maxLeft = 0;
+    let maxRight = 0;
+    let totalWater = 0;
+    while (left < right) {
+        const leftValue = arr[left];
+        if (arr[left] <= arr[right]) {
+            if (maxLeft <= leftValue) {
+                maxLeft = leftValue;
+            } else {
+                totalWater += maxLeft - leftValue;
+            }
+
+            left++;
+        } else {
+            const rightValue = arr[right];
+            if (maxRight <= rightValue) {
+                maxRight = rightValue;
+            } else {
+                totalWater += maxRight - rightValue;
+            }
+
+            right--;
+        }
+    }
+
+    return totalWater;
 };
--- arrays/trappingRainwater.test.js
+++ arrays/trappingRainwater.test.js
@@ -15,3 +15,19 @@ test("when [1] then 0", () => {
 test("when [3,4,3] then 0", () => {
     expect(trappingRainwater([3, 4, 3])).toBe(0);
 });
+
+test("when [0,1,0,2,1,0,1,3,2,1,2,1] then 6", () => {
+    expect(trappingRainwater([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])).toBe(6);
+});
+
+test("when [0,1,0,1] then 1", () => {
+    expect(trappingRainwater([0, 1, 0, 1])).toBe(1);
+});
+
+test("when [0,1,0,2] then 1", () => {
+    expect(trappingRainwater([0, 1, 0, 2])).toBe(1);
+});
+
+test("when [0,1,0,2,0] then 1", () => {
+    expect(trappingRainwater([0, 1, 0, 2, 0])).toBe(1);
+});
