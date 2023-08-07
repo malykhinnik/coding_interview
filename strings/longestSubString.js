306b26e901ee247a2fb5efc3a5fa9dbb709752ee
Nik <malikhinspb@gmail.com>
2023-08-07 09:26:10 +0300

    simple solution longestSubString

--- strings/longestSubString.js
+++ strings/longestSubString.js
@@ -1,4 +1,36 @@
 export default longestSubString;
 
-function longestSubString() {
+function longestSubString(s) {
+    const size = s.length;
+    let max = 0;
+    for (let i = 0; i < size; i++) {
+        let L = s[i];
+        let ri = i + 1;
+        while (ri < size && L !== s[ri]) {
+            ri++;
+        }
+
+        let count = 1;
+        let li = i + 1;
+        while (li < ri) {
+            L = s[li];
+            let newRi = li + 1;
+            while (newRi < ri && L !== s[newRi]) {
+                newRi++;
+            }
+
+            if (newRi < ri) {
+                ri = newRi;
+            } else {
+                li++;
+                count++;
+            }
+        }
+
+        if (count > max) {
+            max = count;
+        }
+    }
+
+    return max;
 }
--- strings/longestSubString.test.js
+++ strings/longestSubString.test.js
@@ -1,5 +1,17 @@
 import longestSubString from "./longestSubString";
+test("when 'a b c c a b b' then 3", () => {
+    expect(longestSubString('abccabb')).toBe(3);
+});
 
-test("when run then undefined", () => {
-    expect(longestSubString()).toBe(undefined);
+test("when 'ccccccc' then 1", () => {
+    expect(longestSubString('ccccccc')).toBe(1);
+});
+
+test("when '' then 0", () => {
+    expect(longestSubString('')).toBe(0);
+});
+
+//          0 1 2 3 4 5
+test("when 'a b c b d a' then 4", () => {
+    expect(longestSubString('abcbda')).toBe(4);
 });
